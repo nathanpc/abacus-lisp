437b32559a9196d2fc85ca072d8253e18df0a44e
Nathan Campos <nathan@innoveworkshop.com>
2025-10-18 09:26:12 +0100

    Started a project in IntelliJ
    A decision was made to build this on top of Java 1.2 and test the Java
    promise of "write once, run anywhere".

---
+++ src/com/innoveworkshop/abacus/Main.java
@@ -0,0 +1,17 @@
+package com.innoveworkshop.abacus;
+
+/**
+ * The application's main runnable class.
+ *
+ * @author Nathan Campos {@literal <nathan@innoveworkshop.com>}
+ */
+public class Main {
+	/**
+	 * Application's main entry point.
+	 *
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
